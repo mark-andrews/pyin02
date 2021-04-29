3d5fc17603d54fd56fd180c5fc277ca604bc8cdf
Mark Andrews <mjandrews.org@gmail.com>
2021-04-29 22:14:28 +0100

    Update recordings and notebook

---
+++ notebooks/analysis.py
@@ -0,0 +1,12 @@
+def power(x = 2, k = 3):
+    """
+    Calculate something to the power of something
+
+    For example power(2, 3) is 2 ** 3....
+
+
+    """
+    return x ** k
+
+def square(x):
+    return x ** 2
